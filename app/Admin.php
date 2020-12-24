f8f70efe4ea58d5c4440963536ccde907ec95616
tomirisarman <48474061+tomirisarman@users.noreply.github.com>
2020-12-24 14:55:42 +0600

    test

---
+++ app/Admin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    use Notifiable;
+
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}

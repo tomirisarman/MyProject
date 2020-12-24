3f5187a7cc1b94b903ab435ce34b4c22f78cfc80
tomirisarman <48474061+tomirisarman@users.noreply.github.com>
2020-12-24 15:55:58 +0600

    Adding admin guard and routes

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,22 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::view('/', 'welcome');
 Auth::routes();
 
-Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm');
-Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm');
+Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm')->name('login.admin');
+Route::get('/login/writer', 'Auth\LoginController@showWriterLoginForm')->name('login.writer');
+Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm')->name('register.admin');
+Route::get('/register/writer', 'Auth\RegisterController@showWriterRegisterForm')->name('register.writer');
 
 Route::post('/login/admin', 'Auth\LoginController@adminLogin');
-
-Route::post('/register/admin', 'Auth\RegisterController@createAdmin');
-
+Route::post('/login/writer', 'Auth\LoginController@writerLogin');
+Route::post('/register/admin', 'Auth\RegisterController@createAdmin')->name('register.admin');
+Route::post('/register/writer', 'Auth\RegisterController@createWriter')->name('register.writer');
 
 Route::view('/home', 'home')->middleware('auth');
-Route::view('/admin', 'admin');
+Route::group(['middleware' => 'auth:admin'], function () {
+    Route::view('/admin', 'admin');
+});
 
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth:writer'], function () {
+    Route::view('/writer', 'writer');
+});

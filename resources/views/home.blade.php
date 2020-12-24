d87eac5333305aa230819962107fbece0f9b3bf1
tomirisarman <48474061+tomirisarman@users.noreply.github.com>
2020-12-24 17:10:18 +0600

    Editing views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
+<div class="sidenav">
+    <a href="#about">About</a>
+    <a href="#services">Services</a>
+    <a href="#clients">Clients</a>
+    <a href="#contact">Contact</a>
+  </div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -20,4 +27,7 @@
         </div>
     </div>
 </div>
+
+</body>
+
 @endsection
